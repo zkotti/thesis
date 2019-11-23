64ff12c63aad0283a038538d0da10a149f03aacc
Zoe Kotti <zoekotti@hotmail.com>
2019-11-23 17:52:01 +0200

    Organize doc files

--- scripts/requirements.py
+++
@@ -1,3 +0,0 @@
-pymongo==3.4.0
-spacy==2.1.4
-google_api_python_client==1.7.9
--- scripts/settings.py
+++
@@ -1,6 +0,0 @@
-from future import print_function
-
-from pymongo import MongoClient
-
-MONGODB_SERVER = ''
-MONGO_CLIENT = MongoClient(host=MONGODB_SERVER)
